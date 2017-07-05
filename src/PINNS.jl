d2f76a3cb9d0c1a2bb86e534a3b1a269b5776a23
Ben J. Ward <ward9250@gmail.com>
2017-07-05 17:37:22 +0100

    Added R preparation

--- src/PINNS.jl
+++ src/PINNS.jl
@@ -4,11 +4,35 @@ using BioSequences
 using RCall
 using BioBridgeR.APE
 
-function prepare_r_session()
+function prepare_r(sequences::Vector{DNASequence}, seed::Int)
     R"""
     library(ape)
     library(phangorn)
+    library(seqinr)
+
+    set.seed($seed)
+
+    SEQUENCES <- as.phyDat($sequences)
+
+    PHYLOGENY <- pratchet(SEQUENCES, trace = 0)
+
+    BASES <- c('a', 'c', 'g', 't')
+
+    ancestryEstimation <- function(seqs, tree){
+        anc <- ancestral.pars(tree, data, "ACCTRAN")
+        m <- anc[[getRoot(tree)]]
+        winners <- apply(m, 1, function(x) sample(BASES, size = 1, prob = x))
+        return(as.DNAbin(winners[attr(anc, "index")]))
+    }
+
+    ANCESTOR <- ancestryEstimation(SEQUENCES, PHYLOGENY)
     """
+end
+
+function PINNS(sequences::Vector{DNASequence}, seed::Int = rand(Int))
+    println("The seed has been set to: ", seed, "...")
+    println("Computing parsimony phylogeny, and computing ancestral sequence...")
+    prepare_r(sequences, seed)
 end
 
 end # Module PINNS.
