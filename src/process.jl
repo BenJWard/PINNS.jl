bd3303006a93b22924dc9c108b79d1431024b127
Ben J. Ward <ward9250@gmail.com>
2017-07-11 13:40:05 +0100

    update how seqs are read in and processed

--- src/process.jl
+++ src/process.jl
@@ -7,11 +7,9 @@ function process(realname::String, simname::String)
         sequence_records = open(realname, "r") do file
             collect(FASTA.Reader(file))
         end
-
-
         println(sequence_records)
+        process_seq_records(output, sequence_records, true)
         exit()
-        write_rep_to_file(output, names, pairwise_dNdS(NG86, seqs), true)
         while !eof(simreader)
             for i in 1:endof(seqs)
                 i
@@ -23,28 +21,17 @@ function process(realname::String, simname::String)
     end
 end
 
+@inline function process_seq_records(output, records, isreal)
+    write_rep_to_file(output,
+                      FASTA.identifier.(records),
+                      pairwise_dNdS(NG86,
+                      FASTA.sequence.(records)),
+                      isreal)
+end
+
 @inline function write_rep_to_file(io, names, results, real)
     @inbounds for i ∈ 1:endof(names), j ∈ (i + 1):endof(names)
         dN, dS = results[i, j]
         println(io, names[i], ", ", names[j], ", ", dN, ", ", dS, ", ", real)
     end
-end
-
-function readreal(filename)
-    open(filename, "r") do file
-        names = Vector{String}()
-        sequences = Vector{DNASequence}()
-        record = FASTA.Record()
-        reader = FASTA.Reader(file)
-        while !eof(reader)
-            read!(reader, record)
-            push!(names, FASTA.identifier(record))
-            push!(sequences, FASTA.sequence(record))
-        end
-        return names, sequences
-    end
-end
-
-function readsim(fh::FASTA.Reader)
-
 end
