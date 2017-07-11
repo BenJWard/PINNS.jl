0782c4dbf993ba7ce498d1a6be6acd11debab27f
Ben J. Ward <ward9250@gmail.com>
2017-07-11 11:07:09 +0100

    print first dnds

--- src/process.jl
+++ src/process.jl
@@ -4,7 +4,8 @@ function process(realname::String, simname::String)
     nseqs = length(seqs)
     println(names)
     println(seqs)
-    compute_dnds(seqs)
+    println(pairwise_dNdS(seqs))
+
     simreader = FASTA.Reader(open(simname, "r"))
     output = open("$(basename(realname)).csv", "w")
 
@@ -13,30 +14,6 @@ function process(realname::String, simname::String)
 
 
 
-end
-
-function compute_dnds(seqs::Vector{DNASequence})
-    for i ∈ 1:endof(seqs), j ∈ i:endof(seqs)
-        icdns, jcdns = aligned_codons(seqs[i], seqs[j])
-        println(icdns)
-        println(jcdns)
-    end
-end
-
-function aligned_codons{T<:NucleicAcidAlphabets}(x::BioSequence{T}, y::BioSequence{T}, start::Int = 1)
-    xcdns = Vector{Kmer{eltype(T), 3}}()
-    ycdns = Vector{Kmer{eltype(T), 3}}()
-    pos = start
-    while pos + 2 ≤ min(endof(x), endof(y))
-        cdnx, okx = BioSequences.extract_kmer_impl(x, pos, 3)
-        cdny, oky = BioSequences.extract_kmer_impl(y, pos, 3)
-        if okx && oky
-            push!(xcdns, convert(Kmer{eltype(T), 3}, cdnx))
-            push!(ycdns, convert(Kmer{eltype(T), 3}, cdny))
-        end
-        pos += 3
-    end
-    return xcdns, ycdns
 end
 
 function readreal(filename)
